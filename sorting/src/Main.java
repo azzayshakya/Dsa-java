59f705179066383024175cde461b99d94b273933
Ajay Kumar Shakya <ajayshakya7376@gmail.com>
2023-08-30 23:02:49 +0530

    bubble sort

---
+++ sorting/src/Main.java
@@ -0,0 +1,28 @@
+
+
+public class Main {
+
+    public static void bubblesort(int arr[]){
+        for (int turn=0;turn<arr.length;turn++){
+            for (int j=0; j<arr.length-1-turn;j++ ){
+                if (arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+
+    public static void printarray(int arr[]){
+        for (int i=0;i<arr.length;i++){
+            System.out.println(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+
+        int arr[]={5,1,3,2,4};
+        bubblesort(arr);
+        printarray(arr);
+    }
+}
