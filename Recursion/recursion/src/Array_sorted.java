85b70f06dd2dbe9bba2426d8fc2e5654b26e6674
Ajay Kumar Shakya <ajayshakya7376@gmail.com>
2023-09-08 16:50:35 +0530

    adding some files

--- Recursion/recursion/src/Array_sorted.java
+++ Recursion/recursion/src/Array_sorted.java
@@ -1,8 +1,17 @@
 public class Array_sorted {
-    public static void issorted(int arr[] ,int i){
-        if
+    public static boolean issorted(int arr[] ,int i){
+        if (arr[i]==arr.length-1){
+            return true;
+
+        }
+        if(arr[i]>arr[i+1]){
+            return false;
+        }
+
+        return issorted(arr,i+1);
     }
-    public static void main(){
-        System.out.println();
+    public static void main(String[] args){
+        int arr[] ={1,4,6,9,8};
+        System.out.println(issorted(arr,0));
     }
 }
