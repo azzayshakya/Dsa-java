8eb3b3e24ad1eb10fbb0321b369dcc0fafb1582c
Ajay Kumar Shakya <ajayshakya7376@gmail.com>
2023-09-10 14:31:24 +0530

    doing some more problems

--- Recursion/recursion/src/remove_duplicates.java
+++ Recursion/recursion/src/remove_duplicates.java
@@ -1,2 +1,20 @@
 public class remove_duplicates {
+    public static void remove(String str ,int idx ,StringBuilder Newstr ,boolean map ){
+        if (idx==str.length()){
+            System.out.println(Newstr);
+            return;
+        }
+        char currind = str.charAt(idx);
+        if(map[currind-'a']==true){
+            remove(str,idx+1,Newstr,map);
+        }
+        else{
+            map[currind-'a']=true;
+            remove(str,idx+1,Newstr.append(currind),map);
+        }
+
+    }
+     public static void main(String[] args){
+
+     }
 }
