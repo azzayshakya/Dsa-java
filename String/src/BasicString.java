f85c209b73a677b0214c62fe5582344b37c7f993
Ajay Kumar Shakya <ajayshakya7376@gmail.com>
2023-09-10 15:13:01 +0530

    doing some basic of String

---
+++ String/src/BasicString.java
@@ -0,0 +1,8 @@
+
+public class BasicString {
+    public static void main(String[] args){
+        String str = "ajay";
+        System.out.println(str);
+
+    }
+}
---
+++ String/src/Basic_String_2.java
@@ -0,0 +1,12 @@
+
+
+import java.util.Scanner;
+public class Basic_String_2 {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        String strg = sc.next();
+
+        System.out.println(strg);
+
+    }
+}
