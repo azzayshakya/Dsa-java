2e220da41732ff0b63e2fc8578b48bd3459cad8c
Ajay Kumar Shakya <ajayshakya7376@gmail.com>
2023-09-10 15:24:35 +0530

    doing some basic of String

--- String/src/Basic_String_2.java
+++ String/src/Basic_String_2.java
@@ -4,9 +4,13 @@ import java.util.Scanner;
 public class Basic_String_2 {
     public static void main(String[] args){
         Scanner sc= new Scanner(System.in);
-        String strg = sc.next();
+        String strg = sc.nextLine();
 
         System.out.println(strg);
+
+
+        // get the legnth of the string
+        System.out.println(strg.length());
 
     }
 }
--- String/src/Basic_String_3.java
+++ String/src/Basic_String_3.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Basic_String_3 {
+
+import java.util.Scanner;
+public class Basic_String_3 {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        String strg = sc.nextLine();
+
+        System.out.println(strg);
+
+
+        System.out.println(strg);
+
+    }
 }
