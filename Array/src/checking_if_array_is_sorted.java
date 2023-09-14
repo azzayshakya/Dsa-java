d2f3cdb8e8a42d148425f68684907ebe9d14b349
Ajay Kumar Shakya <ajayshakya7376@gmail.com>
2023-09-15 03:34:22 +0530

    adding some files

---
+++ Array/src/checking_if_array_is_sorted.java
@@ -0,0 +1,20 @@
+public class checking_if_array_is_sorted {
+
+}
+
+//class Solution {
+//    public boolean check(int[] nums) {
+//        Arrays.sort(nums);
+//        for(int i=1;i<n;i++){
+//            if(nums[i]==nums[i-1]){
+//                return true;
+//            }
+//            return false;
+//
+//        }
+//
+//
+//
+//
+//    }
+//}
---
+++ Array/src/largest_element.java
@@ -0,0 +1,17 @@
+public class largest_element {
+}
+
+
+//#include <bits/stdc++.h>
+//        int largestElement(vector<int> &arr, int n) {
+//        // Write your code here.
+//        int larg = 0;
+//        for (int i = 0; i < n; i++) {
+//        if (arr[i] > larg) {
+//        larg=arr[i];
+//
+//        }
+//
+//        }
+//        return larg;
+//}
