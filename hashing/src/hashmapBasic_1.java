09d1ac149ea5d6247a42fa31e98f82fbb94393fc
Ajay Kumar Shakya <ajayshakya7376@gmail.com>
2023-09-16 17:07:56 +0530

    doing some code

---
+++ hashing/src/hashmapBasic_1.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+public class hashmapBasic_1 {
+
+    public static void main(String[] args) {
+    HashMap<String,Integer> hm= new HashMap<>();
+        hm.put("india",100);
+                hm.put("aus",100);
+                hm.put("eng",100);
+                hm.put("dubai",100);
+
+        System.out.println(hm);
+
+}
+}
