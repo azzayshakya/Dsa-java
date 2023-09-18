443375ba8b862779d5ee04255f584d8e4d54986a
Ajay Kumar Shakya <ajayshakya7376@gmail.com>
2023-09-18 16:51:03 +0530

    asdf

---
+++ hashSet_1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class hashSet_1 {
+    public static void main(String[] args){
+        System.out.println("ajay");
+
+        HashSet<Integer> ab=new HashSet<>();
+        ab.add(23);
+        ab.add(24);
+        ab.add(25);
+        ab.add(26);
+        ab.add(27);
+        System.out.println(ab);
+
+
+
+    }
+}
