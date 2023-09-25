d21d0cbb8e83913f6425dcdc7367c11646c7c177
Ajay Kumar Shakya <ajayshakya7376@gmail.com>
2023-09-25 17:36:33 +0530

    adding some files

--- Recursion/recursion/src/basic_5.java
+++ Recursion/recursion/src/basic_5.java
@@ -1,2 +1,18 @@
 public class basic_5 {
+    public static int NATURALsum(int n){
+        if (n==1){
+            return 1;
+        }
+        NATURALsum(n-1);
+
+        int nat=NATURALsum(n-1);
+        int sum=n + nat;
+        return sum;
+    }
+    public static void main(String[] args){
+
+
+        System.out.println(NATURALsum(5));
+
+    }
 }
