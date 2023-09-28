2b40961a156c3845420ccce8cc94d9b27d26c21f
Ajay Kumar Shakya <ajayshakya7376@gmail.com>
2023-09-28 22:10:23 +0530

    doing some problems

--- Recursion/recursion/src/revision.java
+++ Recursion/recursion/src/revision.java
@@ -1,20 +1,12 @@
 public class revision {
-    public static int fibo(int n){
-        if(n==0 || n==1){
-            return n;
-        }
+    public static int pali(String str ,int s,int e){
+       if();
 
-        fibo(n-1);
-        fibo(n-2);
+       if(String[s)
 
-        int fnm1=fibo(n-1);
-        int fnm2 =fibo(n-2);
-
-        int f=fnm1+fnm2;
-        return f;
     }
 
     public static void main(String[] args) {
-        System.out.println(fibo(10));
+        pali("madam" ,0, 3);
     }
 }
