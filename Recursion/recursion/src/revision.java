b052b39a90479a9ea35cc1bd60f6ed562c088612
Ajay Kumar Shakya <ajayshakya7376@gmail.com>
2023-09-28 11:58:04 +0530

    doing some problems

--- Recursion/recursion/src/revision.java
+++ Recursion/recursion/src/revision.java
@@ -1,14 +1,14 @@
 public class revision {
-    public static int  recursion(int n) {
-        if(n==1){
-            return 1;
+    public static void sorting(int arr[],int s,int e) {
+        if(s>=e){
+            return;
         }
+        int temp =arr[s];
+        arr[s]=arr[e];
+        arr[e]=temp;
 
-        recursion(n-1);
-        int fanm1=recursion(n-1);
-        int fn=n+fanm1;
+        sorting(arr,s+1,e-1);
 
-        return fn;
 
 
 
@@ -16,6 +16,11 @@ public class revision {
     }
     public static void main(String[] args) {
         System.out.println("ajay");
-        System.out.println(recursion(10));
+        int arr[] ={5,3,4,2,1};
+
+        sorting(arr,0,arr.length-1);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
