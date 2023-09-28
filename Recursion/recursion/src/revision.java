a4e84e2144e2e4ca8723ac649106e3062bbf1d27
Ajay Kumar Shakya <ajayshakya7376@gmail.com>
2023-09-28 13:39:22 +0530

    doing some problems

--- Recursion/recursion/src/revision.java
+++ Recursion/recursion/src/revision.java
@@ -1,26 +1,17 @@
 public class revision {
-    public static void sorting(int arr[],int s,int e) {
-        if(s>=e){
-            return;
+    public static void fibo(int n){
+        int num1=0;
+        int num2=1;
+        for(int i=1;i<=n;i++){
+            System.out.println(num1);
+            int sum = num1+num2;
+            num1=num2;
+            num2=sum;
+
         }
-        int temp =arr[s];
-        arr[s]=arr[e];
-        arr[e]=temp;
-
-        sorting(arr,s+1,e-1);
-
-
-
-
-
     }
-    public static void main(String[] args) {
-        System.out.println("ajay");
-        int arr[] ={5,3,4,2,1};
 
-        sorting(arr,0,arr.length-1);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
+    public static void main(String[] args) {
+        fibo(10);
     }
 }
