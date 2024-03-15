c68c927a79e0cc79d8bdc90e28c8098b70d18278
Ajay Kumar Shakya <ajayshakya7376@gmail.com>
2024-03-15 11:15:28 +0530

    doing dsa problems

---
+++ DSA Again -2024/Array/src/arr4_linear_search.java
@@ -0,0 +1,18 @@
+public class arr4_linear_search {
+    static void LinearS(int arr[] , int n, int target){
+        for(int i =0;i<n;i++){
+            if(arr[i]==target){
+                System.out.println("elment found at index "+ i);
+                return ;
+            }
+
+        }
+        System.out.println("element is not available");
+    }
+    public static void main(String[] args) {
+        int arr[]= {1,94,55,23,23,9};
+        int n = arr.length;
+        int target= 9;
+        LinearS(arr,n,target);
+    }
+}
