c4d403406752f1064026a85cd1313f06abd10a75
Ajay Kumar Shakya <ajayshakya7376@gmail.com>
2024-03-17 14:10:29 +0530

    doing dsa

--- DSA Again -2024/Array/src/ar6_left_rotate_arr_by_d_place.java
+++ DSA Again -2024/Array/src/ar6_left_rotate_arr_by_d_place.java
@@ -1,5 +1,23 @@
 public class ar6_left_rotate_arr_by_d_place {
     public static void main(String[] args) {
-        
+
     }
 }
+
+//    static public void  left_rotate_by_one(int arr[],int n){
+//        int temp=arr[0];
+//        for(int i=1;i<n;i++){
+//            arr[i-1]=arr[i];
+//        }
+//        arr[n-1]=temp;
+//        for (int k=0;k<n;k++ ){
+//            System.out.print(arr[k]+" ");
+//        }
+//    }
+//    public static void main(String[] args) {
+//        System.out.println("hy");
+//        int arr[]={2,4,62,6,9,1};
+//        int n =arr.length;
+//        left_rotate_by_one(arr,n);
+//    }
+//}
